458604e864ca64ef8ddaf0ec17f83db18435737a
firuzedurgun <firuzedurgun2001@gmail.com>
2024-04-20 23:22:47 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/NortwindContext.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class NortwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=master;Trusted_Connection=true");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
